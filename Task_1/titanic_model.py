62eea7b761f33e6aee703d55aac61f92de13c17d
Prashant <prashantstha1110@gmail.com>
2024-02-18 13:53:59 +0545

    model for predicition on titanic dataset

---
+++ Task_1/titanic_model.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+# Loading the titanic dataset
+titanic_data = pd.read_csv('/Users/panda/Documents/codsoft/codsoft/Task_1/Titanic-Dataset.csv')
+
+
+# Visualizing the survival counts
+sns.countplot(x='Survived', data=titanic_data)
+plt.title('Survival Count')
+plt.show()
+
+# preprocessing the dataset
+titanic_data['Age'].fillna(titanic_data['Age'].median(), inplace=True)
+titanic_data['Embarked'].fillna(titanic_data['Embarked'].mode()[0], inplace=True)
+titanic_data.drop(['Cabin', 'PassengerId', 'Name', 'Ticket'], axis=1, inplace=True)
+titanic_data = pd.get_dummies(titanic_data, columns=['Sex', 'Embarked'], drop_first=True)
+
+X = titanic_data.drop('Survived', axis=1)
+y = titanic_data['Survived']        # target variable
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# training the model
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+# Confusion matrix
+conf_matrix = confusion_matrix(y_test, y_pred)
+sns.heatmap(conf_matrix, annot=True, fmt='d')
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix')
+plt.show()
+
+# Classification report
+class_report = classification_report(y_test, y_pred)
+print("Classification Report:\n", class_report)
